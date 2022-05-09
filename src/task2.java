7bedd7f43e8226b15f7927dc19a883cb47325b05
Ann <ann.bushl2444@gmail.com>
2022-05-09 15:01:20 +0300

    Задача 2

---
+++ src/task2.java
@@ -0,0 +1,12 @@
+import java.util.Locale;
+
+public class task2 {
+    public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        fullName = fullName.toUpperCase();
+        System.out.println(fullName);
+    }
+}
