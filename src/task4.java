c07bb7f2017bc114bf038280e8e43093b6257a2f
Ann <ann.bushl2444@gmail.com>
2022-05-09 15:28:27 +0300

    Задача 4

---
+++ src/task4.java
@@ -0,0 +1,7 @@
+public class task4 {
+    public static void main(String[] args) {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+    }
+}
